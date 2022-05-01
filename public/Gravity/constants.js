043203c87c573f2619c7365a437dcc696d6eb1f1
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-05-01 15:28:53 +0100

    Replace interval with requestAnimationFrame

--- public/Gravity/constants.js
+++ public/Gravity/constants.js
@@ -1,17 +1,9 @@
-const COLORS = [
-    '#ffffff',
-    '#ff0000',
-    '#ffff00',
-    '#ff00ff',
-    '#00ff00',
-    '#0dcfff',
-];
+const COLORS = ['#ffffff', '#ff0000', '#ffff00', '#ff00ff', '#00ff00', '#0dcfff'];
 
 const G = 1; // Gravitaional constant
 const MAX_STARTING_VELOCITY = 3;
 const BORDER_WIDTH = 5;
 
 const STEP_TIME = 1;
-const INTERVAL = 10;
 
-export { COLORS, G, MAX_STARTING_VELOCITY, BORDER_WIDTH, STEP_TIME, INTERVAL };
+export { COLORS, G, MAX_STARTING_VELOCITY, BORDER_WIDTH, STEP_TIME };
