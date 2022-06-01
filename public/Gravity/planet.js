ceded2e763b5d7afaf83e55a82aafd12ea26295f
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-06-01 21:47:38 +0100

    Update comments

--- public/Gravity/planet.js
+++ public/Gravity/planet.js
@@ -68,6 +68,13 @@ export default class Planet {
         this.div.id = this.id;
     }
 
+    /**
+     * Updates the instance's force vector from interactions with other planets.
+     * @param {Planet[]} planets All other planets to consider in the calculation
+     * @param {Boolean} gravity Whether gravity is enabled
+     * @param {{int, int}} dimensions The effective border
+     * @param {int} minDisplacement Minimum distance between planets at which the calculation will be considered
+     */
     calculateForce(planets, gravity, { height, width }, minDisplacement) {
         let resultantForce = new Vector(0, 0);
 
@@ -94,6 +101,12 @@ export default class Planet {
         this.resultantForce = resultantForce;
     }
 
+    /**
+     * Updates the instance's force vector from a given effective vector.
+     * @param {boolean} gravity
+     * @param {{int, int}} param1
+     * @param {Vector} verticalGravityVector
+     */
     calculateVerticalForce(gravity, { height, width }, verticalGravityVector) {
         if (!gravity) {
             this.resultantForce = new Vector(0, -0).multiply(1);
@@ -105,14 +118,13 @@ export default class Planet {
             return;
         }
 
-        // if (this.position.y < - height + 10 && verticalGravityVector.y < 0) {
-        //     this.resultantForce = new Force(0, 0);
-        //     return;
-        // }
-
         this.resultantForce = verticalGravityVector.multiply(0.5);
     }
 
+    /**
+     * Updates the planet's position and velocity from its resultant force.
+     * @param {{boolean, int, int, double, double}} options
+     */
     step({ bordered, height, width, dampingFactor, delta }) {
         const dv = this.resultantForce.multiply(STEP_TIME);
         this.velocity = this.velocity.add(dv);
@@ -122,16 +134,25 @@ export default class Planet {
 
         const dp = this.velocity.multiply(STEP_TIME);
         this.position = this.position.add(dp);
-
-        const d = {};
-        (d.left = this.div.style.left), (d.top = this.div.style.top);
     }
 
+    /**
+     *
+     * @param {Planet} otherPlanet
+     * @returns {Vector} The displacement from another planet
+     */
     findDisplacement(otherPlanet) {
         const displacement = this.position.findDisplacement(otherPlanet.position);
         return displacement;
     }
 
+    /**
+     * Reflects the current velocity.
+     * @param {int} height Height of bounding box
+     * @param {int} width Width of bounding box
+     * @param {float} dampingFactor Factor in which velocity will be multiplied after bouncing
+     * @param {float} delta Offset from edge of the bounding box which the bounce will occur
+     */
     bounce(height, width, dampingFactor = 1.0, delta = 2) {
         if (this.position.x + this.radius > width - BORDER_WIDTH) {
             this.velocity.x = -dampingFactor * Math.abs(this.velocity.x);
@@ -167,6 +188,11 @@ export default class Planet {
         );
     }
 
+    /**
+     * @param {Planet[]} planetList All other planets
+     * @param {HTMLElement} svgElement The svg element to which annotations will be drawn to
+     * @param {boolean} drawLinesBetweenPlanets
+     */
     addAnnotation(planetList, svgElement, drawLinesBetweenPlanets) {
         for (let i = 0; i < planetList.length; i++) {
             const planet = planetList[i];
@@ -178,10 +204,19 @@ export default class Planet {
         }
     }
 
+    /**
+     * @param {Planet} planet
+     * @param {HTMLElement} svgElement The svg element to which annotations will be drawn to
+     * @returns {HTMLElement} Line which was added to the svgElement
+     */
     addLineToOtherPlanet(planet, svgElement) {
         return addSvgLineFromVectors(this.position, planet.position, svgElement);
     }
 
+    /**
+     * Removes all lines emmitting from this planet
+     * @param {HTMLElement} svgElement The svg element of which to remove the lines from
+     */
     removeLinesFromPlanet(svgElement) {
         Object.values(this.lines).forEach(line => {
             svgElement.removeChild(line);
@@ -189,17 +224,32 @@ export default class Planet {
         this.lines = {};
     }
 
+    /**
+     * Removes all lines emmitting from this planet
+     * @param {Planet} planet The planet from which the lines are coming from
+     * @param {HTMLElement} svgElement The svg element of which to remove the lines from
+     */
     removeLinesToPlanet(planet, svgElement) {
         const line = planet.lines[this.id];
         if (line) svgElement.removeChild(line);
         delete planet.lines[this.id];
     }
 
+    /**
+     * Updates the line positions to match the planets' current position
+     * @param {Planet} planet
+     */
     updateLineToPlanet(planet) {
         const line = this.lines[planet.id];
         if (line) updateSvgLineFromVectors(this.position, planet.position, line);
     }
 
+    /**
+     * Updates the planet's relative position to the window given a change in window position,
+     * such that the absolute screen position remains the same
+     * @param {{double, double}} oldWindowPosition
+     * @param {{double, double}} newWindowPosition
+     */
     updateWindowPosition(oldWindowPosition, newWindowPosition) {
         const dx = newWindowPosition.x - oldWindowPosition.x;
         const dy = newWindowPosition.y - oldWindowPosition.y;
