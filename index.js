7362950792be6bb057ab7799941adde33d849892
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-06-01 21:50:40 +0100

    Move color list to index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { COLORS } from './public/Gravity/constants.js';
 import { Gravity } from './public/Gravity/module.js';
 import { MotionDetector } from './public/Gravity/motionDetector.js';
 import { ConfigStore } from './public/Gravity/configStore.js';
 import UI from './public/uiController.js';
 
+const COLORS = ['#ffffff', '#ff0000', '#ffff00', '#ff00ff', '#00ff00', '#0dcfff'];
 const configStore = new ConfigStore('gravityConfig');
 const motionDetector = new MotionDetector();
 
--- public/Gravity/constants.js
+++ public/Gravity/constants.js
@@ -1,9 +1,7 @@
-const COLORS = ['#ffffff', '#ff0000', '#ffff00', '#ff00ff', '#00ff00', '#0dcfff'];
-
 const G = 1; // Gravitaional constant
 const MAX_STARTING_VELOCITY = 3;
 const BORDER_WIDTH = 5;
 
 const STEP_TIME = 1;
 
-export { COLORS, G, MAX_STARTING_VELOCITY, BORDER_WIDTH, STEP_TIME };
+export { G, MAX_STARTING_VELOCITY, BORDER_WIDTH, STEP_TIME };
